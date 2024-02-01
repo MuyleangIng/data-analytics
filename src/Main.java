1d9d478df13b8a6060428fdf5a9fb8d7f4487284
HoutSovannarith <hout.sovannarith2000@gmail.com>
2024-02-01 11:11:16 +0700

    Sovannarith

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    private static void rith(){
+        System.out.println("Hout Sovannarith");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        rith();
     }
 }
