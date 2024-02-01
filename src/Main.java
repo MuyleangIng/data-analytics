a63b32906f2693f60de90c4847366898b8c15df8
MuyleangIng <muyleanging@gmail.com>
2024-02-01 11:45:38 +0700

    add hello world

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hout Sovannarith");
     }
     public static void main(String[] args) {
-
+        System.out.println("Hello world!v111111");
         System.out.println("Hello world!");
         rith();
         System.out.println("Hello world!");
